6d4005902f0941530bf264b0c12f19e92d4fdcc2
Gabriel Mejia <gabrielm29929@gmail.com>
2022-02-08 18:52:58 -0800

    added both codes for gb and for adding cc

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,19 @@
 // HINT: You can delete this console.log after you no longer need it!
 console.log('JavaScript code has loaded!')
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Gabriel A Mejia" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0 // Gingerbread
 let cc = 0 // Chocolate Chip
 let sugar = 0 // Sugar Sprinkle
+let gbtotal = document.getElementById('qty-gb')
+let cctotal = document.getElementById('qty-cc')
+let sugartotal = document.getElementById('qty-sugar')
+let total = document.getElementById('qty-total')
+let img1 = document.getElementById('first')
+let img2 = document.getElementById('second')
+let img3 = document.getElementById('third')
 
 // selecting the element with an id of credit
 const credit = document.querySelector('#credit')
@@ -22,6 +29,36 @@ gbPlusBtn.addEventListener('click', function() {
 console.log('Gingerbread + button was clicked!')
 
 // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
+document.getElementById('add-gb').addEventListener('click', function() {   
+    gb++
+        gbtotal.textContent=gb 
+        total.textContent=sugar+gb+cc
+        console.log('higb')
+})
+
+/////// code to remove gb
+document.getElementById('minus-gb').addEventListener('click', function() {
+    if(gb > 0)
+    gb--;
+    else;
+    gbtotal.textContent=gb
+    total.textContent=sugar+gb+cc
+    console.log('byegb')
+    if(gb<0)
+    gbtotal.textContent=0;
+    if(gb+cc+sugar<0)
+    total.textContent=0;
+})
+
+
+////// code to add cc
+document.getElementById('add-cc').addEventListener('click', function() {
+    cc++
+    cctotal.textContent=cc
+    total.textContent=sugar+gb+cc
+    console.log('hicc')
+})
+
